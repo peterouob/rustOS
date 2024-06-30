182c259d79beceb5ce2d41f94982b3a6b5344894
林庭楷 <hellolinpeter@gmail.com>
2024-06-30 20:21:21 +0800

    feat:test

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
-use core::fmt::Write;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-    //use core::fmt::Write;
-    //vga_buffer::WRITER.lock().write_str("HEllO again").unwrap();
-    //write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-    // vga_buffer::print_something();
-    println!("Hello World{}","!");
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
---
+++ tests/should_panic.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use blog_os::{QemuExitCode,exit_qemu,serial_println,serial_print};
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    should_failed();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+
+fn should_failed() {
+    serial_print!("should_fail ...");
+    assert_eq!(0,1);
+}
+
+#[panic_handler]
+fn panic(_info : &PanicInfo) -> !{
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+// cargo test --test should_panic
